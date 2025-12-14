b4b01e709d4d75ca20dcafd79942b151010babbf
asli0171 <aslifarrah@gmail.com>
2025-12-14 10:23:29 +0100

    new rep

---
+++ src/test/java/dk/alpha/alphaprojecttool/AlphaProjectToolApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.alpha.alphaprojecttool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaProjectToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
